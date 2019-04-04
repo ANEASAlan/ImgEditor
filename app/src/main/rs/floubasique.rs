4c6295493d7f5ec7d8831a44ae19b95d0d5b2426
nathanbonnaud <nathanbonnaudfac@gmail.com>
2019-04-04 14:25:08 +0200

    flou renderscript fini

--- app/src/main/rs/floubasique.rs
+++ app/src/main/rs/floubasique.rs
@@ -10,30 +10,34 @@ int matrixLength;
 int width;
 int height;
 
-static float4 Value(int x, int y) {
-    float4 value;
+static float4 Value(int indexX, int indexY){
+    float4 value = {0, 0, 0, 255};
 
-        x -= (int) (matrixLength / 2);
-        y -= (int) (matrixLength / 2);
+    indexX -= (int)(matrixLength / 2);
+    indexY -= (int)(matrixLength / 2);
 
-        for (int y2 = 0; y2 < matrixLength; y2++) {
-            for (int x2 = 0; x2 < matrixLength; x2++) {
-                    int tmpx = x + x2;
-                    int tmpy = y + y2;
-                    int finalX = abs((int)(tmpx - (tmpx / (width - 1)) * fmod((float)tmpx, (float)(width - 1)) * 2));
-                    int finalY = abs((int)(tmpy - (tmpy / (height - 1)) * fmod((float)tmpy, (float)(height - 1)) * 2));
-                    float4 pixelf = rsUnpackColor8888(rsGetElementAt_uchar4(in,finalX,finalY));
-                    float coeff = matrix[matrixLength*x+y];
-                    value.r += pixelf.r * coeff;
-                    value.g += pixelf.g * coeff;
-                    value.b += pixelf.b * coeff;
-            }
+    for(int y = 0; y < matrixLength; y++){
+        for(int x = 0; x < matrixLength; x++){
+            int localX = indexX + x;
+            int localY = indexY + y;
+
+            int finalX = abs((int)(localX - (localX / (width - 1)) * fmod((float)localX, (float)(width - 1)) * 2));
+            int finalY = abs((int)(localY - (localY / (height - 1)) * fmod((float)localY, (float)(height - 1)) * 2));
+
+            float4 color = rsUnpackColor8888(rsGetElementAt_uchar4(in, finalX, finalY));
+            float mult = matrix[y + x * matrixLength];
+
+            value.r += color.r * mult;
+            value.g += color.g * mult;
+            value.b += color.b * mult;
 
         }
-        value.a =255;
-        return value;
-
+    }
+    value.a = 255;
+    return value;
 }
+
+
 uchar4 RS_KERNEL floubasique(uint32_t x,uint32_t y){
     float4 pixel = Value(x,y);
     return rsPackColorTo8888(pixel.r , pixel.g , pixel.b , pixel.a);
